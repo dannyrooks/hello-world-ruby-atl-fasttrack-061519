392841b0980d9a840c621d379083318cde1f90ff
Daniel Rooker <dannyrooks@gmail.com>
2019-06-18 23:14:20 +0000

    Automatically backed up by Learn

---
+++ hello_world.rb
@@ -0,0 +1,56 @@
+# puts "Hello World"
+
+# name = "Danny"
+
+# puts name
+
+# number = 6 
+
+# puts number + 3
+
+# pink = "pink"
+
+# puts pink
+
+# light_on = false
+# puts light_on
+# light_on = "true"
+# puts light_on
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# array = [1,2,3]
+
+# login = ["danny", "1234"]
+
+# profile = ["danny", "1234", 2145535303, true]
+# name = "gary"
+# puts name
+# puts profile[2]
+# puts profile[0]
+
+
+
+
+
+puts "Hello World!"
+
+
+
+
+
+
